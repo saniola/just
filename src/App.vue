513f65cba8233db1d3ed04674b6f749342c52c67
Alex Kydanov <a.kidanov@gmail.com>
2024-04-21 21:32:07 +0300

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <header />
+
+    <router-view />
+
+    <footer />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/header.vue';
+import Footer from './components/footer.vue';
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
